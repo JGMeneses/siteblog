e086b0c5de75e279ea9b970fbd01d9e9f2a50e1c
JGMeneses <joaovictorcont15@gmail.com>
2023-05-06 14:22:22 -0300

    MVC_spring

---
+++ src/main/java/blog/siteblog/SiteblogApplication.java
@@ -0,0 +1,13 @@
+package blog.siteblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blog/siteblog/SiteblogApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.siteblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
